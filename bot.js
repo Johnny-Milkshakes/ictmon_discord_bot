2b07fb71a490f4f3a6287c7158f514a72cfb06e6
Alex6323 <Al3k5@protonmail.com>
2019-01-13 02:08:23 +0100

    Made some changes to the formatting.

--- bot.js
+++ bot.js
@@ -8,48 +8,49 @@ console.log('Trying to connect...');
 
 const socket = zmq.socket(`req`);
 socket.connect(`tcp://localhost:5560`);
- 
+
 socket.on('message', function (tps) {
 	console.log('Response received');
-		var data = {
-		  "to": _channelID,
-		  "embed": {
+
+	var data = {
+		"to": _channelID,
+		"embed": {
 			"color": 532392,
 			"fields": [
-			  {
-				"name": "TPS (1 minute)",
-				"value": `${tps}`,
-				"inline": true
-			  },
+				{
+					"name": "TPS (1 minute)",
+					"value": `${tps}`,
+					"inline": true
+				},
 			]
-		  }
+		}
 	};
 	bot.sendMessage(data);
 });
- 
-function sendTpsRequest () {
+
+function sendTpsRequest() {
 	console.log('Sending tps request...');
 	socket.send('tps');
 }
 
 var bot = new discord.Client({
-   token: auth.token,
-   autorun: true
+	token: auth.token,
+	autorun: true
 });
 
 bot.on('ready', function (evt) {
-    console.log('Connected.');
+	console.log('Connected.');
 	console.log('Logged in as: ' + bot.username + ' - (' + bot.id + ')');
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            case 'tps': {
+	if (message.substring(0, 1) == '!') {
+		var args = message.substring(1).split(' ');
+		var cmd = args[0];
+
+		args = args.splice(1);
+		switch (cmd) {
+			case 'tps': {
 				_channelID = channelID;
 				sendTpsRequest();
 				break;
@@ -66,8 +67,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					to: channelID,
 					message: "Yes?"
 				});
-				break;	
+				break;
 			}
-         }
-     }
+		}
+	}
 });
